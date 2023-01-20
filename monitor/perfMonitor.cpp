07caa4bc52a07bef7bf5d0134a50c41277e2c16f
Sangwoong Kim <sw.kim@dgist.ac.kr>
2023-01-21 02:32:32 +0900

    vm: randomly pick metrics to monitor

--- monitor/perfMonitor.cpp
+++ monitor/perfMonitor.cpp
@@ -6,6 +6,7 @@
 #include <cstdio>
 #include <asm/unistd.h>
 #include <sys/mman.h>
+#include <errno.h>
 #include <inttypes.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
@@ -16,6 +17,7 @@
 #include <fstream>
 #include <vector>
 #include <map>
+#include <random>
 #include <boost/algorithm/string/trim.hpp>
 #include <boost/algorithm/string/split.hpp>
 #include <boost/algorithm/string.hpp>
@@ -33,13 +35,21 @@ float membw = 0;
 // kB
 float iobw = 0;
 
-vector<double> mpki(7);
-vector<long> avgs(4);
+vector<double> mpki;
+vector<long> avgs;
 uint64_t cpu_util;
 uint64_t mem_util;
 double ipc;
 double mlp;
 string metric_msg;
+
+int get_random_int(int min, int max) {
+	std::random_device rd;
+	std::mt19937 gen(rd());
+	std::uniform_int_distribution<int> dis(min, max);
+
+	return dis(gen);
+}
 
 int sock_create_connect(const char *ip) {
 	struct sockaddr_in serv_addr;
@@ -78,7 +88,6 @@ string build_metric_msg() {
 
 	res += to_string(cpu_util) + ",";
 	res += to_string(mem_util) + ",";
-	res += to_string(membw) + ",";
 	res += to_string(iobw);
 	
 	return res;
@@ -88,9 +97,9 @@ void read_sock(int sock, string file_name) {
 	char buf[4096];
 	string head_names = "activationId,funcName,";
 	string head_cycle_related = "IPC,MLP,";
-	string head_mpki = "branch,L1D,L1I,LLC,dTLB,iTLB,L2,";
+	string head_mpki = "branch,L1D,L1I,dTLB,iTLB,L2,";
 	string head_avg = "cpuClock,pageFault,cpuMigration,contextSwitch,";
-	string head_others = "cpuUtil,memUtil,memBW,ioBW";
+	string head_others = "cpuUtil,memUtil,ioBW";
 	string header = head_names + head_cycle_related + head_mpki + head_avg + head_others + "\n";
 	int cnt = 0;
 
@@ -271,6 +280,9 @@ struct per_cpu_event {
 
 void accumulate_metrics(struct per_cpu_event *cpu, int num_events) {
 	vector<long long> total_per_event(num_events);
+	avgs.clear();
+	mpki.clear();
+
 	int num_avail_cores = 0;
 
 	for (int i = 0; i < num_cores; i++) {
@@ -291,28 +303,68 @@ void accumulate_metrics(struct per_cpu_event *cpu, int num_events) {
 		return;
 	}
 
+	long total_cycles = total_per_event[0];
 	long total_inst = total_per_event[1];
-	ipc = double(total_inst) / double(total_per_event[0]);
-	mlp = double(total_per_event[13]) / double(total_per_event[14]);
+	long branch_miss = total_per_event[2];
+	
+	long cpu_clock = total_per_event[3];
+	long page_fault = total_per_event[4];
+	long cpu_migration = total_per_event[5];
+	long context_switch = total_per_event[6];
 
-	long avg_clock = total_per_event[3] / num_avail_cores;
+	long l1d_miss = total_per_event[7];
+	long l1i_miss = total_per_event[8];
+	long dtlb_miss = total_per_event[9];
+	long itlb_miss = total_per_event[10];
+
+	long l2_miss = total_per_event[11];
+	long l1d_pending = total_per_event[12];	// for MLP
+	long l1d_pending_cycles = total_per_event[13];	// for MLP
+
+	ipc = double(total_inst) / double(total_cycles);
+	// printf("ipc: %lf\n", ipc);
+	mlp = double(l1d_pending) / double(l1d_pending_cycles);
+
+	long avg_clock = cpu_clock / num_avail_cores;
+	long avg_pagefault = page_fault / num_avail_cores;
+	long avg_cpumigration = cpu_migration / num_avail_cores;
+	long avg_context_switch = context_switch / num_avail_cores;
+	avgs.push_back(avg_clock);
+	avgs.push_back(avg_pagefault);
+	avgs.push_back(avg_cpumigration);
+	avgs.push_back(avg_context_switch);
+	/*
 	for (int i = 3; i < 7; i++) {
 		avgs[i - 3] = total_per_event[i] / num_avail_cores;
 		// avgs.push_back(total_per_event[i] / num_avail_cores);
 	}
+	*/
 
-	double branch_mpki = double(total_per_event[2]) / double(total_inst);
-	mpki[0] = branch_mpki;
-	// mpki.push_back(branch_mpki);
+	double branch_mpki = double(branch_miss) / double(total_inst);
+	double l1d_mpki = double(l1d_miss) / double(total_inst);
+	double l1i_mpki = double(l1i_miss) / double(total_inst);
+	double dtlb_mpki = double(dtlb_miss) / double(total_inst);
+	double itlb_mpki = double(itlb_miss) / double(total_inst);
+	double l2_mpki = double(l2_miss) / double(total_inst);
+
+	mpki.push_back(branch_mpki);
+	mpki.push_back(l1d_mpki);
+	mpki.push_back(l1i_mpki);
+	mpki.push_back(dtlb_mpki);
+	mpki.push_back(itlb_mpki);
+	mpki.push_back(l2_mpki);
+
+	/*
 	for (int i = 7; i < 13; i++) {
 		double tmp_mpki = double(total_per_event[i]) / double(total_inst);
 		mpki[i - 6] = tmp_mpki;
 		// mpki.push_back(tmp_mpki);
 	}
+	*/
 
 	metric_msg = build_metric_msg();
 
-	/*
+	// /*
 	printf("Number of available cores: %d\n", num_avail_cores);
 	printf("==========\n");
 	printf("IPC: %lf\n", ipc);
@@ -326,12 +378,11 @@ void accumulate_metrics(struct per_cpu_event *cpu, int num_events) {
 	printf("Branch misses: %lf\n", mpki[0]);	// MPKI
 	printf("L1D read misses: %lf\n", mpki[1]);	// MPKI
 	printf("L1I read misses: %lf\n", mpki[2]);	// MPKI
-	printf("LLC read misses: %lf\n", mpki[3]);	// MPKI
-	printf("DTLB read misses: %lf\n", mpki[4]);	// MPKI
-	printf("ITLB read misses: %lf\n", mpki[5]);	// MPKI
-	printf("l2_rqsts.all_demand_miss: %lf\n", mpki[6]);	// MPKI
+	printf("DTLB read misses: %lf\n", mpki[3]);	// MPKI
+	printf("ITLB read misses: %lf\n", mpki[4]);	// MPKI
+	printf("l2_rqsts.all_demand_miss: %lf\n", mpki[5]);	// MPKI
 	printf("==========\n");	
-	*/
+	// */
 
 }
 
@@ -349,13 +400,14 @@ int perf_count_multicore(int interval) {
 
 		{ .type = PERF_TYPE_HW_CACHE, .config = (PERF_COUNT_HW_CACHE_L1D) | (PERF_COUNT_HW_CACHE_OP_READ << 8) | (PERF_COUNT_HW_CACHE_RESULT_MISS << 16), .exclude_kernel = 1 },
 		{ .type = PERF_TYPE_HW_CACHE, .config = (PERF_COUNT_HW_CACHE_L1I) | (PERF_COUNT_HW_CACHE_OP_READ << 8) | (PERF_COUNT_HW_CACHE_RESULT_MISS << 16), .exclude_kernel = 1 },
-		{ .type = PERF_TYPE_HW_CACHE, .config = (PERF_COUNT_HW_CACHE_LL) | (PERF_COUNT_HW_CACHE_OP_READ << 8) | (PERF_COUNT_HW_CACHE_RESULT_MISS << 16), .exclude_kernel = 1 },
+		// not work in VM
+		// { .type = PERF_TYPE_HW_CACHE, .config = (PERF_COUNT_HW_CACHE_LL) | (PERF_COUNT_HW_CACHE_OP_READ << 8) | (PERF_COUNT_HW_CACHE_RESULT_MISS << 16), .exclude_kernel = 1 },
 		{ .type = PERF_TYPE_HW_CACHE, .config = (PERF_COUNT_HW_CACHE_DTLB) | (PERF_COUNT_HW_CACHE_OP_READ << 8) | (PERF_COUNT_HW_CACHE_RESULT_MISS << 16), .exclude_kernel = 1 },
 		{ .type = PERF_TYPE_HW_CACHE, .config = (PERF_COUNT_HW_CACHE_ITLB) | (PERF_COUNT_HW_CACHE_OP_READ << 8) | (PERF_COUNT_HW_CACHE_RESULT_MISS << 16), .exclude_kernel = 1 },
 
 		{ .type = PERF_TYPE_RAW, .config = 0x2724, .exclude_kernel = 1 },	// l2_rqsts.all_demand_miss -> exclude prefetch miss
 		{ .type = PERF_TYPE_RAW, .config = 0x148, .exclude_kernel = 1 },		// l1d_pend_miss.pending
-		{ .type = PERF_TYPE_RAW, .config = 0x1000148, .exclude_kernel = 1 },	// l1d_pend_miss.pending_cycles
+		{ .type = PERF_TYPE_RAW, .config = 0x1000148, .exclude_kernel = 1 }	// l1d_pend_miss.pending_cycles
 		/*
 		{ .type = PERF_TYPE_RAW, .config = 0xc0},	// inst_retired.any
 		{ .type = PERF_TYPE_RAW, .config = 0x8d1},	// mem_load_retired.l1_miss
@@ -370,8 +422,9 @@ int perf_count_multicore(int interval) {
 		*/
 	};
 
-	// long num_cores = 1;
+	int max_per_one_read = 12;
 	int num_events = sizeof(pe_def) / sizeof(struct perf_event_attr);
+	int num_exclude = num_events - max_per_one_read;
 
 	struct perf_event_attr pe;
 	struct per_cpu_event *cpu;
@@ -402,29 +455,70 @@ int perf_count_multicore(int interval) {
 	}
 
 	printf("Start monitoring!\n");
-	while(!stop) {
+	int ret;
+	bool *exclude = (bool *)calloc(num_events, sizeof(bool));
 
+	while(!stop) {
+		// clear previous exclude info
+		memset(exclude, false, num_events);
+
+		// pick metric to exclude this iteration
+		int exclude_cnt = 0;
+		while(exclude_cnt < num_exclude) {
+			int tmp = get_random_int(1, 11);
+			if (!exclude[tmp]) {
+				exclude[tmp] = true;
+				exclude_cnt++;
+				printf("%d: %d, ", exclude_cnt, tmp);
+			}
+		}
+		printf("\n");
+
+
+		auto start = chrono::system_clock::now();
 		for (int i = 0; i < num_cores; i++) {
 			for (int j = 0; j < num_events; j++) {
-				ioctl(cpu[i].fd[j], PERF_EVENT_IOC_RESET, 0);
-				ioctl(cpu[i].fd[j], PERF_EVENT_IOC_ENABLE, 0);
+				if (exclude[j]) {
+					continue;
+				}
+
+				ret = ioctl(cpu[i].fd[j], PERF_EVENT_IOC_RESET, 0);
+				if (ret != 0) {
+					printf("PERF_EVENT_IOC_RESET failed! errno: %d\n", errno);
+				}
+				ret = ioctl(cpu[i].fd[j], PERF_EVENT_IOC_ENABLE, 0);
+				if (ret != 0) {
+					printf("PERF_EVENT_IOC_ENABLE failed errno: %d!\n", errno);
+				}
 			}
 		}
 
 		this_thread::sleep_for(chrono::milliseconds(interval));
 
-		auto start = chrono::system_clock::now();
 		for (int i = 0; i < num_cores; i++) {
 			for (int j = 0; j < num_events; j++) {
-				ioctl(cpu[i].fd[j], PERF_EVENT_IOC_DISABLE, 0);
-				read(cpu[i].fd[j], &(cpu[i].count[j]), sizeof(cpu[i].count[j]));
+				if (exclude[j]) {
+					continue;
+				}
+
+				ret = ioctl(cpu[i].fd[j], PERF_EVENT_IOC_DISABLE, 0);
+				if (ret != 0) {
+					printf("PERF_EVENT_IOC_DISABLE failed! errno: %d\n", errno);
+				}
+				ret = read(cpu[i].fd[j], &(cpu[i].count[j]), sizeof(cpu[i].count[j]));
+				if (ret < 0) {
+					printf("read error! errno: %d\n", errno);
+				}
+				else if (ret == 0) {
+					printf("read nothing\n");
+				}
 			}
 		}
 		auto end = chrono::system_clock::now();
 		cpu_util = update_cpu_ns();
 		mem_util = read_mem_util();
 
-		// printf("read duration: %ld us\n", chrono::duration_cast<chrono::microseconds>(end - start).count());
+		// printf("read duration: %ld ms\n", chrono::duration_cast<chrono::milliseconds>(end - start).count());
 		accumulate_metrics(cpu, num_events);
 		// printf("msg: %s\n", metric_msg.c_str());
 	}
@@ -434,6 +528,7 @@ int perf_count_multicore(int interval) {
 		free(cpu[i].count);
 	}
 	free(cpu);
+	free(exclude);
 
 
 	return 0;
@@ -444,6 +539,7 @@ int main(int argc, char *argv[]) {
 	string runtime;
 	string interval;
 
+	/*
 	if (argc > 3) {
 		users = argv[1];
 		runtime = argv[2];
@@ -453,25 +549,26 @@ int main(int argc, char *argv[]) {
 		printf("specify number of users, runtime and monitoring interval!\n");
 		return -1;
 	}
+	*/
 
 	string file_name = "./data/only_metric_" + users + "_" + runtime + "_" + interval + "ms.csv";
-	// int mon_interval = 100;	// ms
-	int mon_interval = stoi(interval);
+	int mon_interval = 100;	// ms
+	// int mon_interval = stoi(interval);
 
-	int sock = sock_create_connect("127.0.0.1");
-	if (sock == -1) {
-		return -1;
-	}
+	// int sock = sock_create_connect("127.0.0.1");
+	// if (sock == -1) {
+	// 	return -1;
+	// }
 
-	thread sock_read(read_sock, sock, file_name);
+	// thread sock_read(read_sock, sock, file_name);
 	thread perf(perf_count_multicore, mon_interval);
-	thread membw(membw_mon, mon_interval);
+	// thread membw(membw_mon, mon_interval);
 	thread iobw(iobw_mon, mon_interval);
 
 	perf.join();
-	membw.join();
+	// membw.join();
 	iobw.join();
-	sock_read.join();
+	// sock_read.join();
 
 	return 0;
 }
