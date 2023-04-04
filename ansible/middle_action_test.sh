603e47034b05e5f40d43b915fbf5604c76ae6232
Sangwoong Kim <sw.kim@dgist.ac.kr>
2023-04-04 11:49:42 +0900

    add scripts

--- ansible/middle_action_test.sh
+++ ansible/middle_action_test.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-for j in {0..100};
+for j in {0..500};
 do
-	for i in {0..100};
+	for i in {0..1};
 	do
 		num=`printf "%.3d" $i`
-		sleep 0.01
+		sleep 0.1
 		../bin/wsk -i action invoke func$num
 	done
 done
