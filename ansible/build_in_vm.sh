3fc3f39b2ff1eb0f04bde06f6a313a0d2ef02bf4
Sangwoong Kim <sw.kim@dgist.ac.kr>
2023-04-15 23:52:21 +0900

    break: only build small number of invokers

--- ansible/build_in_vm.sh
+++ ansible/build_in_vm.sh
@@ -6,9 +6,9 @@ vmip=170
 node_end=6
 port_end=7
 
-for nodes in {0..6}
+for nodes in {0..0}
 do
-	for ip in {0..7}
+	for ip in {0..2}
 	do
 		sudo ssh -p $((3355+$ip)) root@10.150.21.$(($workerip+$nodes)) 'cd /root/openwhisk_metric; ./gradlew :core:invoker:distDocker' &
 	done
